1391fc5cfc56d32bb764a05247430a807c775cd6
olkruglova <olkruhlova@gmail.com>
2018-10-09 22:25:16 +0200

    Add task3

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 var btn = document.querySelector('.btn1');
-var btn2 = document.querySelector('.btn2');
 
+/*
+task1: Даны 3 инпута и кнопка. По нажатию на кнопку получите числа, стоящие в этих инпутах и запишите их сумму в четвертый инпут
+*/
 btn.onclick = function() {
     var number1 = document.querySelector('.input1').value;
     var number2 = document.querySelector('.input2').value;
@@ -12,10 +14,15 @@ btn.onclick = function() {
     number3 = parseInt(number3);
     var result = number1 + number2 + number3;
     results.value = result;
-
+    //...и запишите их сумму в абзац с id="result"
     var sum = document.querySelector('#result');
     sum.innerHTML = '<h3><b>Twój wynik</b>: ' + result +'</h3>';
 }
+
+/*
+task2: Дан инпут. В него вводится число. По нажатии на кнопку найдите сумму цифр этого числа.
+*/
+var btn2 = document.querySelector('.btn2');
 
 btn2.onclick = function() {
     var num = document.querySelector('.input4').value;
@@ -29,3 +36,21 @@ btn2.onclick = function() {
     var sum = document.querySelector('#result2');
     sum.innerHTML = '<h3><b>Twój wynik</b>: ' + summ +'</h3>';
 }
+
+/*
+task3: Дан инпут. В него вводятся числа через запятую. По потери фокуса найдите среднее арифметическое этих чисел
+*/
+var form = document.querySelector(".input5");
+form.addEventListener("blur", function() {
+    var nums = form.value;
+    nums = nums.split(',');
+    console.log(nums)
+    var res = 0;
+    for(var i=0; i<nums.length; i++){
+        nums[i] = parseInt(nums[i]);
+        res += nums[i]/nums.length;
+        console.log(res)
+    }
+    var wyn = document.querySelector('#result3');
+    wyn.innerHTML = '<h3><b>Twój wynik</b>: ' + res +'</h3>';
+}, true);
