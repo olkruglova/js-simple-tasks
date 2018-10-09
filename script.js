0ca9ad11965623c51e42391b0a932a1636d53018
olkruglova <olkruhlova@gmail.com>
2018-10-09 15:06:48 +0200

    Add task2 and some style

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-var btn = document.querySelector('.btn');
+var btn = document.querySelector('.btn1');
+var btn2 = document.querySelector('.btn2');
 
-btn.onclick = function(){
+btn.onclick = function() {
     var number1 = document.querySelector('.input1').value;
     var number2 = document.querySelector('.input2').value;
     var number3 = document.querySelector('.input3').value;
@@ -14,4 +15,17 @@ btn.onclick = function(){
 
     var sum = document.querySelector('#result');
     sum.innerHTML = '<h3><b>Twój wynik</b>: ' + result +'</h3>';
+}
+
+btn2.onclick = function() {
+    var num = document.querySelector('.input4').value;
+    var summ = 0;
+    var res = num.split('');
+    console.log(res);
+    for (var i=0; i<res.length; i++) {
+        res[i] = parseInt(res[i]);
+        summ += res[i];
+    }
+    var sum = document.querySelector('#result2');
+    sum.innerHTML = '<h3><b>Twój wynik</b>: ' + summ +'</h3>';
 }
